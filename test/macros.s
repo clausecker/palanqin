18bb00f40712455d2af9593eff69969523eff2fa
Robert Clausecker <fuz@fuz.su>
2020-07-14 12:58:14 +0200

    define test macros using .inst.n for better disassembly

--- test/macros.s
+++ test/macros.s
@@ -2,17 +2,17 @@
 	.thumb
 
 	.macro	bye
-	.hword	0xb700
+	.inst.n	0xb700
 	.endm
 
 	.macro	debug
-	.hword	0xb701
+	.inst.n	0xb701
 	.endm
 
 	.macro	emit
-	.hword	0xb702
+	.inst.n	0xb702
 	.endm
 
 	.macro	key
-	.hword	0xb703
+	.inst.n	0xb703
 	.endm
