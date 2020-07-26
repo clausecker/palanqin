75229b4f170a8c407e3ffafb0e5b9b2aed3852e6
Robert Clausecker <fuz@fuz.su>
2020-07-27 01:09:15 +0200

    exercise BL in test/hallo.s

--- test/hallo.s
+++ test/hallo.s
@@ -4,7 +4,13 @@
 	.thumb_func
 
 	@ print "Hello World!" using shifts and loads
-_start:	ldr	r0, =0x6c6c6548	@ 'Hell'
+_start:	bl	hell
+	bl	o_wo
+	bl	rld
+	bl	rest
+	bye
+
+hell:	ldr	r0, =0x6c6c6548	@ 'Hell'
 	emit
 	lsrs	r0, r0, #8
 	emit
@@ -12,8 +18,9 @@ _start:	ldr	r0, =0x6c6c6548	@ 'Hell'
 	emit
 	lsrs	r0, r0, #8
 	emit
+	bx	lr
 
-	ldr	r0, =0x6f57206f	@ 'o Wo'
+o_wo:	ldr	r0, =0x6f57206f	@ 'o Wo'
 	emit
 	rev16	r0, r0
 	emit
@@ -21,8 +28,9 @@ _start:	ldr	r0, =0x6c6c6548	@ 'Hell'
 	emit
 	revsh	r0, r0
 	emit
+	bx	lr
 
-	ldr	r0, =0x21646c72	@ 'rld!'
+rld:	ldr	r0, =0x21646c72	@ 'rld!'
 	str	r0, [sp, #0]
 	mov	r1, sp
 	movs	r0, '?'
@@ -34,16 +42,17 @@ _start:	ldr	r0, =0x6c6c6548	@ 'Hell'
 	emit
 	rev16	r0, r0
 	emit
+	bx	lr
 
 	@ print a string using a loop
-	adr	r1, string
+rest:	adr	r1, string
 0:	ldrb	r0, [r1]
 	adds	r1, r1, #1
 	cmp	r0, #0
 	beq	1f
 	emit
 	b	0b
-1:	bye
+1:	bx	lr
 
 	.balign	4
 string:	.string	"\r\nJedem Anfang wohnt ein Zauber inne."
